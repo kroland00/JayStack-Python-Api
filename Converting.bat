8cd7bc2e4a22e2012eca128c3d2b000e4712db14
Roland Kigyósi <75250514+kroland00@users.noreply.github.com>
2020-11-30 14:26:37 +0100

    Update Converting.bat

--- Converting.bat
+++ Converting.bat
@@ -1,1 +1,1 @@
-start e:\melo\Github_repos\JayStack-Python-Api\converter_env\Scripts\python.exe -i e:\melo\Github_repos\JayStack-Python-Api\video_converter.py
+start PATH TO VENV PYTHON EXE -i PATH TO PYTHON FILE TO BE RUNNED
